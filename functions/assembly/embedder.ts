4a0289134ba91c3a6e07eddb680daeb1b89b70dd
rderbier <rderbier@gmail.com>
2024-06-25 14:14:45 -0700

    Update embedder.ts

--- functions/assembly/embedder.ts
+++ functions/assembly/embedder.ts
@@ -3,11 +3,11 @@ import { EmbeddingsModel } from "@hypermode/models-as/models/experimental/embedd
 
 const modelName = "minilm";
 
-export function embedIssue(text: string): f64[] {
+export function embedIssue(text: string[]): f32[][] {
   const model = models.getModel<EmbeddingsModel>(modelName);
 
-  const input = model.createInput([text]);
+  const input = model.createInput(text);
   const output = model.invoke(input);
 
-  return output.predictions[0].map<f64>((x) => x);
+  return output.predictions;
 }
