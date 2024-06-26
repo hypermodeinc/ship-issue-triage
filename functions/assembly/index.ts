33ddb9d46b024de74e25dcd5d85faf12aa6a22fb
Matt Johnson-Pint <mjp@hypermode.com>
2024-06-25 18:53:55 -0700

    Remove spam classification

--- functions/assembly/index.ts
+++ functions/assembly/index.ts
@@ -1,7 +1,4 @@
 export { trendSummary } from "./trends";
 export { classifyIssue } from "./classify";
-
 export { minilmEmbedder } from "./embedder";
 export { similarIssues, addIssue } from "./similar";
-
-export { isSpam, addSpam } from "./spam";
